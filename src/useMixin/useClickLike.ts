4eb2296d793abe054610c0832a29c7555e3a2656
wall <854067626@qq.com>
2021-11-28 22:58:45 +0800

    完善点赞逻辑

--- src/useMixin/useClickLike.ts
+++ src/useMixin/useClickLike.ts
@@ -1,17 +1,19 @@
 import { ref, computed } from "vue";
+import { Toast } from "vant";
 
 function useClickLike(requestApi: Function) {
   let currentId = ref(""); // 当前id
   let isLike = ref(false); // 是否点赞
+  let isLikeSuccess = ref(true); // 是否点赞成功
   let likeList = ref<string[]>([]); // 点过赞列表
   // 获取点赞数
   const getLikesNumber = computed(
     () => (id: string, likes: number) =>
-      likeList.value.includes(id) ? likes + 1 : likes
+      isLikeSuccess.value && likeList.value.includes(id) ? likes + 1 : likes
   );
   // 点赞高亮
   const getLikesColor = computed(
-    () => (id: string) => likeList.value.includes(id)
+    () => (id: string) => isLikeSuccess.value && likeList.value.includes(id)
   );
 
   const handleLikes = (id: string) => {
@@ -23,9 +25,15 @@ function useClickLike(requestApi: Function) {
       likeList.value.push(id);
     }
     currentId.value = id;
-    return requestApi({ _id: id, isLike: isLike.value }).catch((err: any) => {
-      console.log(err);
-    });
+    return requestApi({ _id: id, isLike: isLike.value })
+      .then(() => {
+        isLikeSuccess.value = true;
+      })
+      .catch((err: any) => {
+        isLikeSuccess.value = false;
+        Toast("点赞失败");
+        console.log(err);
+      });
   };
 
   return {
